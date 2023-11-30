5ddd50ff3571a3e98e30ca8d7a6e03d5d33aa139
vidyakumari123 <127233800+vidyakumari123@users.noreply.github.com>
2023-11-30 14:06:33 +0530

    first commit

---
+++ src/components/Assignment.jsx
@@ -0,0 +1,157 @@
+import React from 'react';
+import './Product.css';
+
+function ProductCard() {
+    let arr = [
+        {
+            "id": 2,
+            "title": "iPhone X",
+            "description": "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+            "price": 899,
+            "discountPercentage": 17.94,
+            "rating": 4.44,
+            "stock": 34,
+            "brand": "Apple",
+            "category": "smartphones",
+            "thumbnail": "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
+            "images": [
+                "https://i.dummyjson.com/data/products/2/1.jpg",
+                "https://i.dummyjson.com/data/products/2/2.jpg",
+                "https://i.dummyjson.com/data/products/2/3.jpg",
+                "https://i.dummyjson.com/data/products/2/thumbnail.jpg"
+            ]
+        },
+        {
+            "id": 3,
+            "title": "Samsung Universe 9",
+            "description": "Samsung's new variant which goes beyond Galaxy to the Universe",
+            "price": 1249,
+            "discountPercentage": 15.46,
+            "rating": 4.09,
+            "stock": 36,
+            "brand": "Samsung",
+            "category": "smartphones",
+            "thumbnail": "https://i.dummyjson.com/data/products/3/thumbnail.jpg",
+            "images": [
+                "https://i.dummyjson.com/data/products/3/1.jpg"
+            ]
+        },
+        {
+            "id": 4,
+            "title": "OPPOF19",
+            "description": "OPPO F19 is officially announced on April 2021.",
+            "price": 280,
+            "discountPercentage": 17.91,
+            "rating": 4.3,
+            "stock": 123,
+            "brand": "OPPO",
+            "category": "smartphones",
+            "thumbnail": "https://i.dummyjson.com/data/products/4/thumbnail.jpg",
+            "images": [
+                "https://i.dummyjson.com/data/products/4/1.jpg",
+                "https://i.dummyjson.com/data/products/4/2.jpg",
+                "https://i.dummyjson.com/data/products/4/3.jpg",
+                "https://i.dummyjson.com/data/products/4/4.jpg",
+                "https://i.dummyjson.com/data/products/4/thumbnail.jpg"
+            ]
+        },
+        {
+            "id": 5,
+            "title": "Huawei P30",
+            "description": "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
+            "price": 499,
+            "discountPercentage": 10.58,
+            "rating": 4.09,
+            "stock": 32,
+            "brand": "Huawei",
+            "category": "smartphones",
+            "thumbnail": "https://i.dummyjson.com/data/products/5/thumbnail.jpg",
+            "images": [
+                "https://i.dummyjson.com/data/products/5/1.jpg",
+                "https://i.dummyjson.com/data/products/5/2.jpg",
+                "https://i.dummyjson.com/data/products/5/3.jpg"
+            ]
+        },
+        {
+            "id": 6,
+            "title": "MacBook Pro",
+            "description": "MacBook Pro 2021 with mini-LED display may launch between September, November",
+            "price": 1749,
+            "discountPercentage": 11.02,
+            "rating": 4.57,
+            "stock": 83,
+            "brand": "Apple",
+            "category": "laptops",
+            "thumbnail": "https://i.dummyjson.com/data/products/6/thumbnail.png",
+            "images": [
+                "https://i.dummyjson.com/data/products/6/1.png",
+                "https://i.dummyjson.com/data/products/6/2.jpg",
+                "https://i.dummyjson.com/data/products/6/3.png",
+                "https://i.dummyjson.com/data/products/6/4.jpg"
+            ]
+        },
+        {
+            "id": 7,
+            "title": "Samsung Galaxy Book",
+            "description": "Samsung Galaxy Book S (2020) Laptop With Intel Lakefield Chip, 8GB of RAM Launched",
+            "price": 1499,
+            "discountPercentage": 4.15,
+            "rating": 4.25,
+            "stock": 50,
+            "brand": "Samsung",
+            "category": "laptops",
+            "thumbnail": "https://i.dummyjson.com/data/products/7/thumbnail.jpg",
+            "images": [
+                "https://i.dummyjson.com/data/products/7/1.jpg",
+                "https://i.dummyjson.com/data/products/7/2.jpg",
+                "https://i.dummyjson.com/data/products/7/3.jpg",
+                "https://i.dummyjson.com/data/products/7/thumbnail.jpg"
+            ]
+        }
+    ];
+
+    const categories = ['All', 'smartphone', 'Laptop', 'Accessories'];
+    const brands = ['All', 'Nike', 'Adidas', 'Puma'];
+
+    return (
+        <div className="container">
+            <h1></h1>
+            <div className="sidebar">
+                <h2>Categories</h2>
+                <ul>
+                    {categories.map((category, i) => (
+                        <li>{category}</li>
+                    ))}
+                </ul>
+
+                <h2>Brands</h2>
+                <ul>
+                    {brands.map((brand, i) => (
+                        <li>{brand}</li>
+                    ))}
+                </ul>
+            </div>
+
+            <div className="product-container">
+                {arr.map((item, i) => (
+                    <div className="card" key={i}>
+                        <img src={item.thumbnail} />
+                        <div className="card-content">
+                            <div className="card-header">{item.prodName}</div>
+                            <div className="price">${item.price}</div>
+                            <div className="category-brand">
+                                <span>Category: {item.category}</span>
+                                <span>Brand: {item.brand}</span>
+                            </div>
+                            <div>
+                                <button>Add to Cart</button>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default ProductCard;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ProductCard from './components/Assignment';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <ProductCard/>
+    {/* <App /> */}
   </React.StrictMode>
 );
 
